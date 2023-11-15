ee82d3ca4c6762f7fb45c87f11d8ea75e015ca6b
Lorenzo Reyes <akirafierro@gmail.com>
2023-11-15 17:42:05 -0300

    Add files via upload

---
+++ pilimage.py
@@ -0,0 +1,16 @@
+import cv2, pytesseract
+import numpy as np
+from PIL import Image
+
+# Load the image
+image = Image.open("topa.jpeg")
+text = pytesseract.image_to_string(image)
+
+cv2.imwrite('topeado.jpeg', result) 
+
+# function to apply and check changes on the go
+def preview(name,effect):
+    cv2.imwrite(f'{name}.jpeg', effect)
+    img = Image.open(f'{name}.jpeg')
+    return img
+print(text)
